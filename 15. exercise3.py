e2bd5549ca07bb875f9e1ebcdb42ccb4280ebc02
Jhonatan-K <120041475+Jhonatan-K@users.noreply.github.com>
2023-01-21 09:36:26 -0500

    Add files via upload

---
+++ 15. exercise3.py
@@ -0,0 +1,9 @@
+# exercise 3 intercambiar el valor de 2 variables
+
+a = input("ingrese primer valor:")
+b = input("ingrese segundo valor:")
+
+a , b = b , a
+
+print(f"el nuevo valor de a es:{a}")
+print(f"el nuevo valor de b es:{b}")
